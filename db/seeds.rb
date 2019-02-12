dff342025bfde6dab61fd69b3f539b11ac418c90
Xeniia Gavrilenko <X.gavrilenko@gmail.com>
2019-02-12 22:41:23 +0100

    Created an index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+10.times do
+  Article.create!(
+    title: Faker::DcComics.title,
+    content: Faker::Hipster.sentence,
+  )
+end
+
